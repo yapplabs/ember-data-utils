30bd6057ca7d73d39de8d461001d4e757537f89c
Matt McManus <matt@mcmanus.io>
2019-02-19 09:22:57 -0500

    Fix documentation for QueryData component

--- addon/components/query-data/component.js
+++ addon/components/query-data/component.js
@@ -1,5 +1,3 @@
-/** @documenter esdoc */
-
 import Component from '@ember/component';
 import layout from './template';
 import { parseQueryToFunction } from 'ember-data-utils/utils/parse-query-to-function';
@@ -9,33 +7,50 @@ import { task } from 'ember-concurrency';
 import { inject as service } from '@ember/service';
 
 /**
-   A component to fetch data from your api
+  A component to fetch data from your api
 
-   ```handlebars
-    <QueryData
-      @modelType='app-page-profile'
-      @sortBy={{array 'createdAt:desc'}}
-      @query={{hash pageId=@page.id}}
-    as |data|>
-        ...
-    </QueryData>
-    ```
-    @argument {string} modelType - name of the ember data model
-    @argument {array} sortBy - Array of keys to sort by
-    @argument {object} query - Object of values to query against the server
-    @argument {function} onModelsLoaded - Callback that is fired when models are loaded
-
-    @yield {object} data
-    @yield {boolean} data.isLoading
-    @yield {boolean} data.isEmpty
-    @yield {array} data.sortedModels - The models returned by the api, sorted
-    @yield {Action} data.refresh - Action method to trigger the component to refresh the data
+  ```handlebars
+  {{query-data
+    modelType='app-page-profile'
+    sortBy=(array 'createdAt:desc')
+    query=(hash pageId=@page.id)
+    as |data|}}
+    ...
+  {{/query-data}}
+  ```
+  @class QueryData
+  @yield {object} data
+  @yield {boolean} data.isLoading
+  @yield {boolean} data.isEmpty
+  @yield {array} data.sortedModels The models returned by the api, sorted
+  @yield {Action} data.refresh Action method to trigger the component to refresh the data
 */
 export default Component.extend({
   layout,
 
   tagName: '',
   store: service(),
+
+  /**
+    The name of the ember data model, eg; 'user'
+    @argument {string} modelType
+    @required
+  */
+  modelType: null,
+
+  /**
+    The object of values to query against the server, eg: { admin: true, firstName: 'Bob' }
+    @argument {object} query
+    @required
+  */
+  query: null,
+
+  /**
+    Callback fired every time server response finished
+    @argument {Function} onModelsLoaded
+    @optional
+  */
+  onModelsLoaded() {},
 
   didReceiveAttrs() {
     this._super(...arguments);
@@ -50,6 +65,16 @@ export default Component.extend({
 
   sortedModels: sort('filteredModels', 'sortBy'),
 
+  /**
+    A live record array of all of the models in the store of that
+    type. This array will be automatically filtered down by the
+    filter function created from the query hash
+
+    @computed liveRecordArray
+    @type Array<Object>
+    @private
+    @readOnly
+  */
   liveRecordArray: computed('modelType', function() {
     return this.store.peekAll(this.modelType);
   }),
@@ -59,6 +84,12 @@ export default Component.extend({
     if (this.onModelsLoaded) { this.onModelsLoaded(); }
   }).keepLatest(),
 
+  /**
+    @computed filteredModels
+    @type Array<Object>
+    @private
+    @readOnly
+  */
   filteredModels: computed('liveRecordArray.[]', 'query', 'isLoading', function() {
       let filterFn = this.buildFilterFunction(this.query);
 
@@ -66,6 +97,12 @@ export default Component.extend({
     }
   ),
 
+  /**
+    @method buildFilterFunction
+    @param {object} query - Query hash passed into `query` method
+    @return {Function}
+    @private
+  */
   buildFilterFunction(query) {
     let queryFunc = parseQueryToFunction(query);
     return function(record) {
@@ -74,6 +111,11 @@ export default Component.extend({
   },
 
   actions: {
+    /**
+      @action refresh
+      @public
+      @return {Task} - An instance of an Ember Concurrency Task
+    */
     refresh() {
       this.loadDataTask.perform();
     }
