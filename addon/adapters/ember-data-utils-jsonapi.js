dce1f76f42b13130335f01dae322071504ebc019
Matt McManus <matt@mcmanus.io>
2019-02-19 09:43:37 -0500

    Document EmberDataUtilsJSONAPIAdapter

--- addon/adapters/ember-data-utils-jsonapi.js
+++ addon/adapters/ember-data-utils-jsonapi.js
@@ -1,15 +1,52 @@
-/** @documenter esdoc */
 import DS from 'ember-data';
 import { camelize } from '@ember/string';
 import { isEmpty } from '@ember/utils';
 
 /**
- * Test
+  Class to set as your ApplicationAdapter. Extend it for each model 
+  you want to configure explicit filter and include params. 
+  
+  ```js
+  // app/adapters/application.js
+  import { EmberDataUtilsJSONAPIAdapter } from 'ember-data-utils';
+
+  export default EmberDataUtilsJSONAPIAdapter.extend({});
+  ```
+
+  ```js
+  // app/adapters/post.js
+  import BaseAdapter from './application';
+
+  export default BaseAdapter.extend({
+    supportedFilters: ['page-id'],
+    include: ['image', 'profile.image']
+  })
+  ```
+  @class EmberDataUtilsJSONAPIAdapter
  */
 export default DS.JSONAPIAdapter.extend({
+  /**
+    List of query param keys that your resource supports filtering on. 
+    Extend your base class for specific models to declare. 
+    @field {Array} supportedFilters
+   */
   supportedFilters: [], // eslint-disable-line ember/avoid-leaking-state-in-ember-objects
+
+  /**
+    List of includes to always include when querying a particular model.
+    Extend your base class for specific models to declare. 
+    
+    @field {Array} include
+   */
   include: [], // eslint-disable-line ember/avoid-leaking-state-in-ember-objects
   
+  /**
+   * @public
+   * @method query
+   * @param {Store} store Instance of your ember data store
+   * @param {string} type A string of the name of your model, eg 'post'
+   * @param {object} query Hash to mutate into JSONAPI query params
+   */
   query(store, type, query) {
     let jsonApiQuery = {};
     jsonApiQuery = this._applyPagination(jsonApiQuery, query);
@@ -19,6 +56,17 @@ export default DS.JSONAPIAdapter.extend({
     return this._super(store, type, jsonApiQuery);
   },
 
+  /**
+   * Mutate the `jsonApiQueryParams` object with pagination related keys from `rawQueryParams` object
+   * 
+   * Pagination related keys are `limit`, `since`, and `until`
+   * 
+   * @private
+   * @method _applyPagination
+   * @param {Object} jsonApiQueryParams Object of JSONAPI formated query params
+   * @param {Object} rawQueryParams Original, unformated Object of query params
+   * @return {Object} Mutated jsonApiQueryParams object with pagination params applied 
+   */
   _applyPagination(jsonApiQueryParams, rawQueryParams) {
     ['limit', 'since', 'until'].forEach(key => {
       let value = rawQueryParams[`_${key}`];
