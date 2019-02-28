7021fd42e8cd4d30ab6a63236add4bbdfc579aa0
Matt McManus <matt@mcmanus.io>
2019-02-28 11:55:37 -0500

    Rename adapter for consistency, cleanup export

--- addon/index.js
+++ addon/index.js
@@ -1,5 +1,7 @@
-import EmberDataUtilsJSONAPIAdapter from 'ember-data-utils/adapters/ember-data-utils-jsonapi';
+import EmberDataUtilsJSONAPIAdapter from 'ember-data-utils/adapters/ember-data-utils-json-api';
+import EmberDataUtilsJSONAPISerializer from 'ember-data-utils/serializers/ember-data-utils-json-api';
 
 export {
-  EmberDataUtilsJSONAPIAdapter
+  EmberDataUtilsJSONAPIAdapter,
+  EmberDataUtilsJSONAPISerializer
 }
